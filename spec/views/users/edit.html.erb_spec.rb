ea37666241ec65554b2a8f375f286df4de4dfa3d
Israel Rivas <israelrivasgonzalez@gmail.com>
2019-04-18 15:12:03 +0200

    Adding static pages and user views

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -1,30 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "users/edit", type: :view do
+RSpec.describe 'users/edit', type: :view do
   before(:each) do
     @user = assign(:user, User.create!(
-      :name => "MyString",
-      :surname => "MyString",
-      :email => "MyString",
-      :age => 1,
-      :role => "MyString"
+      name: 'MyString',
+      surname: 'MyString',
+      email: 'MyString',
+      age: 1,
+      role: 'MyString'
     ))
   end
 
-  it "renders the edit user form" do
+  it 'renders the edit user form' do
     render
-
-    assert_select "form[action=?][method=?]", user_path(@user), "post" do
-
-      assert_select "input[name=?]", "user[name]"
-
-      assert_select "input[name=?]", "user[surname]"
-
-      assert_select "input[name=?]", "user[email]"
-
-      assert_select "input[name=?]", "user[age]"
-
-      assert_select "input[name=?]", "user[role]"
+    assert_select 'form[action=?][method=?]', user_path(@user), 'post' do
+      assert_select 'input[name=?]', 'user[name]'
+      assert_select 'input[name=?]', 'user[surname]'
+      assert_select 'input[name=?]', 'user[email]'
+      assert_select 'input[name=?]', 'user[age]'
+      assert_select 'input[name=?]', 'user[role]'
     end
   end
 end
